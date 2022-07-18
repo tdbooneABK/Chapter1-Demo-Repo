12d4845b7e6012feda44457a28c7d454c2242264
tdbooneABK <trevor.boone@activision.com>
2022-07-18 16:42:04 -0700

    Remove boilerplate comments.

--- Chapter1-Demo/Chapter1-Demo/Chapter1-Demo.cpp
+++ Chapter1-Demo/Chapter1-Demo/Chapter1-Demo.cpp
@@ -1,6 +1,3 @@
-// Chapter1-Demo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 using namespace std;
@@ -22,14 +19,3 @@ int main()
 	cout << "Here's the sum:" << endl << sum << endl;
 	cout << "Here's the average:" << endl << ((float)sum) / 3.0 << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
